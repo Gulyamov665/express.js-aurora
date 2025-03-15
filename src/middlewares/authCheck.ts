783c99b25a7b5304174a49edc83ca7bc988c102a
Gulyamov M.A <gulyamov@buroburo.ru>
2025-03-15 15:56:50 +0500

    move django access token to dotenv in express

--- src/middlewares/authCheck.ts
+++ src/middlewares/authCheck.ts
@@ -1,18 +1,21 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 if (!process.env.JWT_KEY) {
   throw new Error("JWT_KEY is not defined in environment variables");
 }
 
 // const JWT_SECRET = process.env.JWT_KEY; // Нужно взять из Django settings.py
-const JWT_SECRET = "django-insecure-pl+(h+sfdiotagz&rz&lo45w^qa)j0b=sbcfv&r1(&##n-44s%"; // Нужно взять из Django settings.py
+const JWT_SECRET = String(process.env.SECRET_KEY); // Нужно взять из Django settings.py
 
 interface AuthenticatedRequest extends Request {
   user?: string | JwtPayload;
 }
 
-console.log(JWT_SECRET)
+console.log(JWT_SECRET);
 
 export const authenticateJWT = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
