a8bb26d2712df211bff859f52a33d2e8da24c178
Gulyamov M.A <gulyamov@buroburo.ru>
2025-03-15 15:47:50 +0500

    add cors for stage2

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: ["http://localhost:3000", "http://localhost:5173", "https://stage.aurora-app.uz/"],
+  origin: ["http://localhost:3000", "http://localhost:5173", "https://stage.aurora-app.uz"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
