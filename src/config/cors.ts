8be9084b4d412973797116679465154f14cae786
Gulyamov M.A <gulyamov@buroburo.ru>
2025-03-21 13:02:25 +0500

    cors settings change

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,8 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: ["http://localhost:3000", "http://localhost:5173", "https://stage.aurora-app.uz"],
+  // origin: ["http://localhost:3000", "http://localhost:5173", "https://stage.aurora-app.uz"],
+  origin: ["*"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -9,7 +10,8 @@ const corsMiddleware = cors({
 
 export const WebSocketCors = {
   cors: {
-    origin: "http://localhost:5173", // Укажи URL фронта (или "*", если тестируешь локально)
+    // origin: "http://localhost:5173", // Укажи URL фронта (или "*", если тестируешь локально)
+    origin: "*", // Укажи URL фронта (или "*", если тестируешь локально)
     methods: ["GET", "POST"],
   },
 };
