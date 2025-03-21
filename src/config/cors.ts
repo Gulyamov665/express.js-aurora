b1f2db23ec40d1d99e7ccbafd061d257a5b02dd2
Gulyamov M.A <gulyamov@buroburo.ru>
2025-03-21 13:18:31 +0500

    change corses v2

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,8 +1,13 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  // origin: ["http://localhost:3000", "http://localhost:5173", "https://stage.aurora-app.uz"],
-  origin: ["*"],
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://stage.aurora-app.uz",
+    "https://aurora-3avt-git-feature-ts-gulyamov-mirzogulyams-projects.vercel.app/",
+  ],
+
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
@@ -11,7 +16,12 @@ const corsMiddleware = cors({
 export const WebSocketCors = {
   cors: {
     // origin: "http://localhost:5173", // Укажи URL фронта (или "*", если тестируешь локально)
-    origin: ["*"], // Укажи URL фронта (или "*", если тестируешь локально)
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://stage.aurora-app.uz",
+      "https://aurora-3avt-git-feature-ts-gulyamov-mirzogulyams-projects.vercel.app/",
+    ], // Укажи URL фронта (или "*", если тестируешь локально)
     methods: ["GET", "POST"],
   },
 };
