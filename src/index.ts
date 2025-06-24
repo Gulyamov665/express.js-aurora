e0e81f12ba570b0148f08750f7e1eb2f93b2e027
Gulyamov M.A <gulyamov@buroburo.ru>
2025-06-24 20:15:50 +0500

    fix: enhance type safety by specifying Socket type in WebSocket connection

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import app from "./app";
 import { WebSocketCors } from "./config/cors";
 import { connectDB } from "./config/database";
 import http from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 export const io = new Server(server, WebSocketCors);
+
+// 1) Хранилище pending таймеров
+interface Pending {
+  timer: NodeJS.Timeout;
+}
+const pending = new Map<string, Pending>();
 
 const startServer = async () => {
   if (process.env.NODE_ENV !== "test") {
@@ -18,8 +24,8 @@ const startServer = async () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
   });
 
-  io.on("connection", (socket) => {
-    console.log(`🛜 WS Клиент подключен: ${socket.id}`);
+  io.on("connection", (socket: Socket) => {
+    console.log(`🛜 WS Клиент подключен: ${socket.id} ${socket}`);
 
     socket.on("disconnect", () => {
       console.log(`🔌 Клиент отключился: ${socket.id}`);
